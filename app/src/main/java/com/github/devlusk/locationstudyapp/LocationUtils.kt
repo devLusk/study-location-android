b71c26c579d7229d204760e13e4eb3be324873c8
Lucas <lucasvida2005@gmail.com>
2025-12-03 12:24:20 -0300

    Add utility to check location permissions

---
+++ app/src/main/java/com/github/devlusk/locationstudyapp/LocationUtils.kt
@@ -0,0 +1,23 @@
+package com.github.devlusk.locationstudyapp
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+class LocationUtils(private val context: Context) {
+
+    fun hasLocationPermission(): Boolean {
+        return (
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+                ) || (
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+                )
+    }
+}
